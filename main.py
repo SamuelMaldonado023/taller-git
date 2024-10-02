55d26f39fa32b36a072388e0b4edb2becc524541
SamuelMaldonado023 <Samuel.maldonado3@upr.edu>
2024-10-02 21:20:41 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main():
     print("Asistencia para taller de Git:")
-    print("1. Diego A. Estrada Rivera")
+    print("1. Samuel J. Maldonado Batista")
     print("2. El Perro")
     print("3. El Goat")
 
